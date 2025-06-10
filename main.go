623f123a3773c733cd4b2697feec7ded5f36a5dc
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2025-06-09 20:41:34 -0500

    feat: implement logger

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fm-go-vanillajs-movies/logger"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hey")
+	// Initialize logger
+	logInstance := initializeLogger()
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = ":8080"
+	logInstance.Info("Server starting on" + addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		logInstance.Error("Server failed to start", err)
+		log.Fatalf("Server has failed: %v", err)
+	}
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie-service.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	return logInstance
 }
